814e8ce930aef79e70b7ba9f33b1c0663c770bc5
Mehmet Demirbas <mehmet1demirbas@gmail.com>
2026-02-06 12:30:33 +0300

    Add heap sort implementation and update project files

--- bubble_sort_1.c
+++ bubble_sort_1.c
@@ -1,27 +1,25 @@
-#include<stdio.h>
-#include"nutility.h"
+// #include<stdio.h>
+#include "nutility.h"
 
-#define				SIZE		100
+#define SIZE 100
 
-int main()
-{
-	int a[SIZE];
-	randomize();
-	set_random_array(a, SIZE);
-	print_array(a, SIZE);
+int main() {
+  int a[SIZE];
+  randomize();
+  set_random_array(a, SIZE);
+  print_array(a, SIZE);
 
-	for (int i = 0; i < SIZE - 1 ; i++)
-	{
-		for (int k = 0; k < SIZE - i - 1; k++)
-		{
-			if (a[k]%2==0 && a[k+1]%2==1 || a[k]%2==a[k+1]%2 && a[k] > a[k + 1]) {
+  for (int i = 0; i < SIZE - 1; i++) {
+    for (int k = 0; k < SIZE - i - 1; k++) {
+      if (a[k] % 2 == 0 && a[k + 1] % 2 == 1 ||
+          a[k] % 2 == a[k + 1] % 2 && a[k] > a[k + 1]) {
 
-				int temp = a[k];
-				a[k] = a[k + 1];
-				a[k + 1] = temp;
-			}
-		}
-	}
+        int temp = a[k];
+        a[k] = a[k + 1];
+        a[k + 1] = temp;
+      }
+    }
+  }
 
-	print_array(a, SIZE);
+  print_array(a, SIZE);
 }
---
+++ heap_sort.c
@@ -0,0 +1,74 @@
+﻿#include <stdio.h>
+#include "nutility.h"
+#include <time.h>
+#include <conio.h>
+#include <stdlib.h>
+
+static void swap_int(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+static void heapify(int* arr, int n, int i)
+{
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < n && arr[left] > arr[largest])
+		largest = left;
+
+	if (right < n && arr[right] > arr[largest])
+		largest = right;
+
+	if (largest != i)
+	{
+		swap_int(&arr[i], &arr[largest]);
+		heapify(arr, n, largest);
+	}
+}
+
+static void heap_sort(int* arr, int n)
+{
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(arr, n, i);
+
+	for (int i = n - 1; i > 0; i--)
+	{
+		swap_int(&arr[0], &arr[i]);
+		heapify(arr, i, 0);
+	}
+}
+
+int main()
+{
+	int n;
+	printf("Kac eleman: ");
+	(void)scanf("%d", &n);
+	if (n <= 0)
+	{
+		printf("Gecersiz eleman sayisi\n");
+		return 1;
+	}
+
+	int* pd = (int*)malloc(n * sizeof(int));
+	if (!pd)
+	{
+		printf("Bellek yetersiz\n");
+		return 1;
+	}
+
+	set_random_array(pd, n);
+
+	clock_t start = clock();
+	heap_sort(pd, n);
+	clock_t end = clock();
+
+	printf("Siralama bitti %f saniye\n", (double)(end - start) / CLOCKS_PER_SEC);
+	_getch();
+	print_array(pd, n);
+	free(pd);
+	return 0;
+}
